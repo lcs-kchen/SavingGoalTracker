fc3d7f925224f4210afe6d84005216445139cbc4
Kevin Chen <kchen@lcs.on.ca>
2025-02-27 09:28:53 -0500

    Initial Commit

---
+++ SavingGoalTracker/SavingGoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingGoalTrackerApp.swift
+//  SavingGoalTracker
+//
+//  Created by Haowen Chen on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct SavingGoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
